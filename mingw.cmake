969c0987649270cbdbc4e389bbcbbc315fa1ec85
Yawin <tuzmakel@gmail.com>
2024-03-25 19:00:52 +0100

    Se adaptan ficheros de compilación para que la versión de windows compile con POSIX

--- mingw.cmake
+++ mingw.cmake
@@ -1,12 +1,19 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
 
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc-posix)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
+#set(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
@@ -15,3 +22,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # search headers and libraries in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+ADD_DEFINITIONS(-DWIN)
